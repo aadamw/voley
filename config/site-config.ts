b79a5d06f634cd10dce3b7657f7ded3f4b022542
Adam Wojtycki <adamwojtycki@gmail.com>
2023-11-08 21:12:18 +0100

    Add shadcn, change nextconfig

---
+++ config/site-config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  title: 'Voley',
+  description: 'Changelog template for your projects',
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
