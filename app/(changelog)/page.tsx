c87b88ccd077e6a322d20efd767db7f326f8614a
__aadamw <131662061+aadamw@users.noreply.github.com>
2023-11-10 14:50:18 +0100

    [Feat] Display list of posts on home page (#14)

--- app/(changelog)/page.tsx
+++ app/(changelog)/page.tsx
@@ -1,17 +1,25 @@
-import { Footer } from '@/app/footer';
-import { Navigation } from '@/components/navigation';
-import { H1 } from '@/components/ui/typography';
+import { H1, H2 } from '@/components/ui/typography';
+import { getAllPosts } from '@/lib/posts';
+import Link from 'next/link';
 
 export default function Home() {
+  const posts = getAllPosts();
+
   return (
     <>
-      <Navigation />
-      <main className="mt-4 flex-grow px-4 md:mt-16 md:px-6">
-        <div className="mx-auto md:w-2/3">
-          <H1>Changelog</H1>
-        </div>
-      </main>
-      <Footer />
+      <div className="mx-auto md:w-2/3">
+        <H1>Changelog</H1>
+        <section className="mt-10 grid gap-4 ">
+          {posts.map((post, idx) => (
+            <article key={idx} className="border-b py-4">
+              <H2>
+                <Link href={post.url}>{post.title}</Link>
+              </H2>
+              <p>This is test description</p>
+            </article>
+          ))}
+        </section>
+      </div>
     </>
   );
 }
