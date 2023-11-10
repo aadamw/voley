6df84462b4102ae8b37143417c1af32634b96d89
aadamw <adamwojtycki@gmail.com>
2023-11-10 15:34:23 +0100

    Extend contentlayer type

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,12 +1,28 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import { defineDocumentType, defineNestedType, makeSource } from 'contentlayer/source-files';
+
+const Author = defineNestedType(() => ({
+  name: 'author',
+  fields: {
+    name: { type: 'string', required: true, description: 'Name of the author' },
+    avatarUrl: { type: 'string', required: true, description: 'Avatar image of the author' },
+  },
+}));
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: `**/*.mdx`,
   contentType: 'mdx',
   fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
+    title: { type: 'string', required: true, description: 'Title of the post' },
+    date: { type: 'date', required: true, description: 'Date when post is published' },
+    description: { type: 'mdx', required: true, description: 'Display post summary on' },
+    coverImgUrl: { type: 'string', required: true, description: 'Cover image of the post' },
+    authors: {
+      type: 'list',
+      of: Author,
+      required: true,
+      description: 'List of authors who contributed to this post',
+    },
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/${post._raw.flattenedPath}` },
