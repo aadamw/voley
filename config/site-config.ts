9a7985e626107f4a3f8d8796bbe658c948517dc1
aadamw <adamwojtycki@gmail.com>
2023-11-14 10:05:20 +0100

    chore: update site-config

--- config/site-config.ts
+++ config/site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'Voley',
   description: 'Changelog template for your projects',
-  url: `https://${process.env.VERCEL_URL}` ?? 'http://localhost:3000',
+  url: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
 } satisfies SiteConfig;
 
 export const footer = {
